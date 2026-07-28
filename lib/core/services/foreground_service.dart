6999c8c1d83bb14bcd849f86ec7dd0abac25231a
bsfdsagfadg <918272281@qq.com>
2026-07-28 20:20:30 +0800

    fix: 补充 foreground_service.dart 缺失的 dart:convert 引用

--- lib/core/services/foreground_service.dart
+++ lib/core/services/foreground_service.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:convert';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'dart:async';
 import 'package:flutter_pty/flutter_pty.dart';
