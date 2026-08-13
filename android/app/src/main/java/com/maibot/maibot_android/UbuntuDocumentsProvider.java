78e0f33ea4084fc75cf22786a9acc1bc1022ed90
bsfdsagfadg <918272281@qq.com>
2026-08-13 18:22:59 +0800

    fix(android): add missing import for IOException in UbuntuDocumentsProvider

--- android/app/src/main/java/com/maibot/maibot_android/UbuntuDocumentsProvider.java
+++ android/app/src/main/java/com/maibot/maibot_android/UbuntuDocumentsProvider.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.LinkedList;
 
